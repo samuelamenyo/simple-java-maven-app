668fa4081fd297835819a96fc63bbfc9b72055dc
samuelamenyo <94507950+samuelamenyo@users.noreply.github.com>
2024-09-23 09:42:02 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! SAMUEL AMENYO IS LEARING JENKINS";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
